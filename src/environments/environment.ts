3a6ae473a89a03e73906e2061fb55618c8955647
axelfoxII <asfoxII@gmail.com>
2022-08-23 20:01:46 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   apiMarvel:'https://gateway.marvel.com:443/v1/public',
-  public_key:'85acd8e40c99614f64f7e05bfd8b24a7',
-  hash:'fbe64d60b8ae2e7534af81442bb50d8f',
+  public_key:'SU API KEY',
+  hash:'DEBEN GENERAR SU HASH --> timestamp + private key + public key',
   production: false
 };
 
